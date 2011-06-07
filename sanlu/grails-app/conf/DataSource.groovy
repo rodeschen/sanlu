f5019abae0c545495edac11c7b87a3dfea1f7d6a
sedor.leman <sedor.leman@bce4a620-458e-c844-f683-5f353072547c>
2011-06-07 22:35:10 +0000

    git-svn-id: http://sanlu.googlecode.com/svn/trunk@117 bce4a620-458e-c844-f683-5f353072547c

--- sanlu/grails-app/conf/DataSource.groovy
+++ sanlu/grails-app/conf/DataSource.groovy
@@ -1,9 +1,23 @@
 dataSource {
+
+	//logSql = true	
 	pooled = true
+	dbCreate = "update"
+	url = "jdbc:mysql://localhost/yourDB"
 	driverClassName = "com.mysql.jdbc.Driver"
-	username = "root"
+    username = "root"
 	password = ""
-	logSql = true	
+	properties {
+		maxActive = 50
+		maxIdle = 25
+		minIdle = 5
+		initialSize = 5
+		minEvictableIdleTimeMillis = 60000
+		timeBetweenEvictionRunsMillis = 60000
+		maxWait = 10000
+		validationQuery = "/* ping */"
+	}
+
 }
 hibernate {
 	cache.use_second_level_cache=true
@@ -15,6 +29,7 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			//jndiName = "java:comp/env/jdbc/sanlu"
 			url = "jdbc:mysql://localhost/sanlu?useUnicode=true&characterEncoding=UTF-8"
 		}
 	}
